0ea6ac2cf512bfb7c3e19bdfbf60cd3a93bdc9e8
arumora-id <arumoraid@gmail.com>
2026-09-12 15:14:18 +0700

    fix: load Windi virtual stylesheet from Vite entry

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter } from 'react-router-dom'
+import 'virtual:windi.css'
 import { App } from './app/App'
 import './styles/base.css'
 
